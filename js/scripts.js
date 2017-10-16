42edaa78a9cfae920dede6ad4d47bd8517296cf4
Nathan Barrett and Sam Kissee <nathan@>
2017-10-16 10:20:30 -0700

    add folders and html doc

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var subtract = function subtract(number1, number2) {return number1 - number2;};
+
+var popup = function(whatToSay) {alert(whatToSay);};
+
+var multiply = function multiply(number1, number2) {return number1 * number2;};
+
+
+var threetimes = function threetimes(number1, number2, number3) {return number1 * number2 * number3;};
+
+var divide = function divide(number1, number2) {return number1 / number2;};
+var remainder = function remainder(number1, number2) {return number1 % number2};
+
+var add = function(number1, number2) {return number1 + number2;};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
