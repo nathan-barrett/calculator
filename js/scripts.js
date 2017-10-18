4411579ba3970080618f18aeae9da1324006bb38
Nathan Barrett and Tyler Bussanich <tyler@>
2017-10-18 11:30:56 -0700

    set up forms with add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var popup = function(whatToSay) {alert(whatToSay);};
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -12,7 +12,11 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 $(document).ready(function() {
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  alert(add(number1, number2));
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
 });
